631d95ce7129fb6435c7e1ba37ca48c2b73f2aee
Demarcus Mcneese <92759558+Demarcus3456@users.noreply.github.com>
2022-06-15 08:08:36 -0500

    Add files via upload

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,1 @@
 // Menu
-
-const list = document.querySelectorAll('.list');
-function activeLink() {
-    list.forEach((item) =>
-        item.classList.remove('active'));
-    this.classlist.add('active');
-}
-list.forEach((item) =>
-    item.addEventListener('click', activeLink));
