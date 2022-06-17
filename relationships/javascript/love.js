24d40c2443c1ddcd23414367ae33e23d6c731825
“Demarcus <dcolemand@ymail.com>
2022-06-17 10:38:33 -0500

    p

---
+++ relationships/javascript/love.js
@@ -0,0 +1,10 @@
+let number = document.getElementById("number");
+let counter = 0;
+setInterval(() => {
+    if (counter == 0) {
+        clearInterval();
+    } else {
+        counter += 1;
+        number.innerHTML = counter + "%"
+    }
+}, 30);
