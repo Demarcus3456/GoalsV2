7bfeed2c1cdb571e0fb4740a062e2ddc1ea5153c
Demarcus Mcneese <92759558+Demarcus3456@users.noreply.github.com>
2025-03-28 14:04:00 -0500

    Update to home page

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,11 @@
-// Menu
+'use strict'
+
+const agreement = document.querySelector('.agreement');
+const agreeBtn = document.querySelector('.agreebtn');
+const content = document.querySelector('.content');
+
+/* Showing content after agreement */
+agreeBtn.addEventListener('click', function(){
+    agreement.classList.add('hidden');
+    content.classList.remove('hidden');
+});
