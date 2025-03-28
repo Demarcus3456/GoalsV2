049808bff6c1027e91a21b1ca67035a8cb54207b
Demarcus Mcneese <92759558+Demarcus3456@users.noreply.github.com>
2025-03-28 14:35:09 -0500

    Update #2

--- javascript/script.js
+++ javascript/script.js
@@ -3,9 +3,18 @@
 const agreement = document.querySelector('.agreement');
 const agreeBtn = document.querySelector('.agreebtn');
 const content = document.querySelector('.content');
+const step = document.querySelector('.container')
+const navbar = document.querySelector('.navbar');
+const menu = document.querySelector('.menu');
 
 /* Showing content after agreement */
 agreeBtn.addEventListener('click', function(){
     agreement.classList.add('hidden');
     content.classList.remove('hidden');
+});
+
+/* Opens menu */
+menu.addEventListener('click', function(){
+    step.classList.add('hidden');
+    navbar.classList.remove('hidden');
 });
